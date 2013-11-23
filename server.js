6a5f5ea544a2f57d4cac1cd9e819bbdb806a41a2
sprengmeistr <sebastian.sprenger@outlook.com>
2013-11-23 15:10:35 +0100

    express api

--- server.js
+++ server.js
@@ -4,16 +4,51 @@ var app = express();
 var authentication = require('./authentication');
 var locations = require('./locations');
 
+var port = process.env.PORT || 8099;
+
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({secret: 'ec52b70f-7235-4358-a8f2-9065e5344cbb'}));
 
+app.get('/', function(req, res){
+	var body = 'Hello World';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
+});
+
+app.post('/login', function(req, res){
+	var user = req.body.user,
+		password = req.body.password;
+	authentication.login(req.session, user, password);
+});
+
+app.post('/locations', function(req, res){
+	if (!authentication.loggedIn(req.session))
+		return;
+
+	var latitude  = req.body.latitude,
+	    longitude = req.body.longitude;
+	locations.addLocation(latitude, longitude);
+});
+
+app.get('/locations', function(req, res){
+	if (!authentication.isLoggedIn(req.session))
+		return;
+
+	var data = locations.getLocations();
+	res.write(JSON.stringify(data));
+});
+
+app.listen(port);
+console.log('listening on port ' + port);
+/*
 var http = require('http')
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8099;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port)
+}).listen(port)*/
 
 /*var express = require('express');
 var app = express();
